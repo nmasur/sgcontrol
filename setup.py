7c96b5c2cb392ec3a8ba54a3c0fef2885bc5c9f8
Noah Masur <nmasur@wesleyan.edu>
2017-02-28 15:31:07 -0500

    New package with command-line script

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from distutils.core import setup
 setup(
   name = 'sgcontrol',
   packages = ['sgcontrol'], # this must be the same as the name above
-  version = '0.5',
+  version = '0.5.1',
   description = 'Foolproof AWS security group management',
   author = 'Noah Masur',
   author_email = 'nmasur@wesleyan.edu',
+  scripts=['bin/sgcontrol'],
   url = 'https://github.com/nmasur/sgcontrol', # use the URL to the github repo
-  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.1.tar.gz',
+  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.5.1.tar.gz',
   keywords = ['aws', 'aws-security', 'devops'], # arbitrary keywords
   classifiers = [],
 )
