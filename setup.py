100b0dfbce6f1aa599ff6b63e73f514b79bffadd
Noah Masur <nmasur@wesleyan.edu>
2017-02-28 15:55:29 -0500

    Fix import problems, maybe.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 setup(
   name = 'sgcontrol',
   packages = ['sgcontrol'], # this must be the same as the name above
-  version = '0.5.3',
+  version = '0.5.4',
   description = 'Foolproof AWS security group management',
   author = 'Noah Masur',
   author_email = 'nmasur@wesleyan.edu',
   entry_points={
-      'console_scripts': ['sgcontrol = sgcontrol:main']
+      'console_scripts': ['sgcontrol = sgcontrol.sgcontrol:main']
   },
   url = 'https://github.com/nmasur/sgcontrol', # use the URL to the github repo
-  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.5.3.tar.gz',
+  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.5.4.tar.gz',
   keywords = ['aws', 'aws-security', 'devops'], # arbitrary keywords
   classifiers = [],
   install_requires=['boto', 'PyYAML', 'ec2']
