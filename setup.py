3d693e594c56ad81733092cd74b830417f665ff2
Noah Masur <nmasur@wesleyan.edu>
2017-02-28 15:19:11 -0500

    Removing install requires

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
   packages = ['sgcontrol'], # this must be the same as the name above
   version = '0.5',
   description = 'Foolproof AWS security group management',
-  install_requires=['boto', 'ec2', 'PyYAML'],
   author = 'Noah Masur',
   author_email = 'nmasur@wesleyan.edu',
   url = 'https://github.com/nmasur/sgcontrol', # use the URL to the github repo
