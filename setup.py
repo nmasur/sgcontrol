aa1d9a627aab09dd218c8777e25406131d839a05
Noah Masur <nmasur@wesleyan.edu>
2017-02-28 15:01:13 -0500

    Changing directory to package style.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+  name = 'sgcontrol',
+  packages = ['sgcontrol'], # this must be the same as the name above
+  version = '0.5',
+  description = 'Foolproof AWS security group management',
+  install_requires=['boto', 'ec2', 'PyYAML'],
+  author = 'Noah Masur',
+  author_email = 'nmasur@wesleyan.edu',
+  url = 'https://github.com/nmasur/sgcontrol', # use the URL to the github repo
+  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.1.tar.gz',
+  keywords = ['aws', 'aws-security', 'devops'], # arbitrary keywords
+  classifiers = [],
+)
