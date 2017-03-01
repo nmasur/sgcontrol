51f889492d8810abf870c4efeffbc92ca66720e7
Noah Masur <nmasur@wesleyan.edu>
2017-03-01 11:19:26 -0500

    Next release, fixed bugs.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'sgcontrol',
   packages = ['sgcontrol'], # this must be the same as the name above
-  version = '0.5.6',
+  version = '0.5.7',
   description = 'Foolproof AWS security group management',
   author = 'Noah Masur',
   author_email = 'nmasur@wesleyan.edu',
@@ -12,7 +12,7 @@ setup(
       'console_scripts': ['sgcontrol = sgcontrol.sgcontrol:main']
   },
   url = 'https://github.com/nmasur/sgcontrol', # use the URL to the github repo
-  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.5.6.tar.gz',
+  download_url = 'https://github.com/nmasur/sgcontrol/archive/0.5.7.tar.gz',
   keywords = ['aws', 'aws-security', 'devops'], # arbitrary keywords
   classifiers = [],
   install_requires=['boto', 'PyYAML', 'ec2']
